df82be8e63e32ad5737caa7ddd812e5ef13631d9
Prabal Bhandari <prabalb046348@gmail.com>
2022-07-25 14:48:00 +0545

    genesis

--- controllers/userController.js
+++ controllers/userController.js
@@ -169,6 +169,57 @@ const userCtrl = {
             return res.status(500).json({msg: err.message})
         }
     },
+
+    googleLogin: async (req, res) => {
+        try {
+            const {tokenId} = req.body
+
+            const verify = await client.verifyIdToken({idToken: tokenId, audience: process.env.MAILING_SERVICE_CLIENT_ID})
+            
+            const {email_verified, email, name, picture} = verify.payload
+
+            const password = email + process.env.GOOGLE_SECRET
+
+            const passwordHash = await bcrypt.hash(password, 12)
+
+            if(!email_verified) return res.status(400).json({msg: "Email verification failed."})
+
+            const user = await Users.findOne({email})
+
+            if(user){
+                const isMatch = await bcrypt.compare(password, user.password)
+                if(!isMatch) return res.status(400).json({msg: "Password is incorrect."})
+
+                const refresh_token = createRefreshToken({id: user._id})
+                res.cookie('refreshtoken', refresh_token, {
+                    httpOnly: true,
+                    path: '/user/refresh_token',
+                    maxAge: 7*24*60*60*1000 // 7 days
+                })
+
+                res.json({msg: "Login success!"})
+            }else{
+                const newUser = new Users({
+                    name, email, password: passwordHash, avatar: picture
+                })
+
+                await newUser.save()
+                
+                const refresh_token = createRefreshToken({id: newUser._id})
+                res.cookie('refreshtoken', refresh_token, {
+                    httpOnly: true,
+                    path: '/user/refresh_token',
+                    maxAge: 7*24*60*60*1000 // 7 days
+                })
+
+                res.json({msg: "Login success!"})
+            }
+
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
 };
 
 
